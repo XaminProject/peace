69616378de6cab58b0404a8484fb7ac44ae47259
behrooz shabani (everplays) <behrooz@rock.com>
2012-07-11 15:16:39 +0430

    replaced 4 characters indention with 2 characters

--- src/main/scala/ir/xamin/Peace.scala
+++ src/main/scala/ir/xamin/Peace.scala
@@ -7,29 +7,28 @@ class Peace(host: Option[String], username: Option[String], password: Option[Str
     println(username.get+"@"+host.get+"/"+resource.getOrElse("peace"))
 }
 
-object Peace
-{
-    import org.clapper.argot._
-    import org.clapper.argot.ArgotConverters._
+object Peace {
+  import org.clapper.argot._
+  import org.clapper.argot.ArgotConverters._
 
-    val parser = new ArgotParser("Peace", preUsage=Some("Version 0.0.1"))
+  val parser = new ArgotParser("Peace", preUsage=Some("Version 0.0.1"))
 
-    val host = parser.parameter[String]("hostname", "xmpp server", false)
-    val username = parser.parameter[String]("username", "username pf jid", false)
-    val password = parser.parameter[String]("password", "password to be used for authentication", false)
-    val resource = parser.parameter[String]("resource", "resource of jid", true)
+  val host = parser.parameter[String]("hostname", "xmpp server", false)
+  val username = parser.parameter[String]("username", "username pf jid", false)
+  val password = parser.parameter[String]("password", "password to be used for authentication", false)
+  val resource = parser.parameter[String]("resource", "resource of jid", true)
 
-    def main(args: Array[String])
+  def main(args: Array[String])
+  {
+    try
     {
-        try
-        {
-            parser.parse(args)
-            new Peace(host.value, username.value, password.value, resource.value)
-        }
-        catch
-        {
-            case e: ArgotUsageException => println(e.message)
-        }
+      parser.parse(args)
+      new Peace(host.value, username.value, password.value, resource.value)
     }
+    catch
+    {
+      case e: ArgotUsageException => println(e.message)
+    }
+  }
 }
 
