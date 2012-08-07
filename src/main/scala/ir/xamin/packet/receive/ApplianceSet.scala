145224304efb520c4a531f780cb87f1c80e16640
behrooz shabani (everplays) <behrooz@rock.com>
2012-08-07 17:41:42 +0430

    added apidoc of receive ApplianceSet

--- src/main/scala/ir/xamin/packet/receive/ApplianceSet.scala
+++ src/main/scala/ir/xamin/packet/receive/ApplianceSet.scala
@@ -6,6 +6,9 @@ import ir.xamin.Appliance
 import scala.xml._
 import org.jivesoftware.smack.packet.IQ
 
+/** this class ables rms to tell peace about new appliances
+ * and new version of them
+ */
 class ApplianceSet extends IQ {
   private var name:String = _
   private var description:String = _
@@ -14,32 +17,72 @@ class ApplianceSet extends IQ {
   private var author:String = _
   private var tags:List[String] = _
 
+  // set type of iq
   setType(IQ.Type.SET)
 
+  /** getter for name of appliance that will be stored
+   * @return a string which is name of appliance
+   */
   def getName = name
 
+  /** setter for name of appliance that will be stored
+   * @param v a string that is name of appliance
+   */
   def setName(v:String) = name = v
 
+  /** getter for description of appliance that will be stored
+   * @return a string which is name of appliance
+   */
   def getDescription = description
 
+  /** setter for description of appliance that will be stored
+   * @param v a string which is description of appliance
+   */
   def setDescription(v:String) = description = v
 
+  /** getter for version of appliance that will be stored
+   * @return a string which is version of appliance
+   */
   def getVersion = version
 
+  /** setter for version of appliance that will be stored
+   * @param v a string which is version of appliance
+   */
   def setVersion(v:String) = version = v
 
+  /** getter for url of xvm2 of appliance that will be stored
+   * @return a string which is url of xvm2 file of appliance
+   */
   def getURL = url
 
+  /** setter for url of xvm2 of appliance that will be stored
+   * @param v a string which is url of xmv2 file of appliance
+   */
   def setURL(v:String) = url = v
 
+  /** getter for author of appliance that will be stored
+   * @return a string which is author of appliance
+   */
   def getAuthor = author
 
+  /** setter for author of appliance that will be stored
+   * @param v a string which is author of appliance
+   */
   def setAuthor(v:String) = author = v
 
+  /** getter for tags of appliance that will be stored
+   * @return a list of tags
+   */
   def getTags = tags
 
+  /** setter for tags of appliance that will be stored
+   * @param v the list of tags
+   */
   def setTags(v:List[String]) = tags = v
 
+  /** the inner xml of iq
+   * @return a string which is going to be inner xml of iq
+   */
   def getChildElementXML:String = {
     val ns = ApplianceSetProvider.namespace
     val _tags = tags.flatMap { s => <tag>{s}</tag> }
@@ -53,6 +96,9 @@ class ApplianceSet extends IQ {
     </package>.toString
   }
 
+  /** creates reply to request
+   * @return the reply
+   */
   def buildReply:ReplyApplianceSet = {
     val applianceSet = new ReplyApplianceSet
     applianceSet setPacketID getPacketID
