4805b9fe54e12484765278e7f0153ee6dd647478
behrooz shabani (everplays) <behrooz@rock.com>
2012-08-07 17:09:16 +0430

    added apidoc of request EnableAppliance

--- src/main/scala/ir/xamin/packet/receive/ApplianceEnable.scala
+++ src/main/scala/ir/xamin/packet/receive/ApplianceEnable.scala
@@ -6,20 +6,43 @@ import ir.xamin.Appliance
 import scala.xml._
 import org.jivesoftware.smack.packet.{IQ, Packet}
 
+/** a class for enabling an appliance in redis
+ *
+ * after creating an appliance in RMS it tells peace to enable
+ * appliance which to say so send us an IQ stanza that this
+ * class represents
+ */
 class ApplianceEnable extends IQ {
   private var name:String = _
   private var version:String = _
 
+  // set type of IQ
   setType(IQ.Type.GET)
 
+  /** getter for name of appliance
+   * @return an string which is name of appliance that has been
+   * requested to get enabled
+   */
   def getName = name
 
+  /** setter for name of appliance
+   * @param value the name of appliance that should be enabled
+   */
   def setName(value: String):Unit = name = value
 
+  /** getter for version of appliance
+   * @return an string which is version of appliance that has been
+   * requested to get enabled
+   */
   def getVersion = version
 
+  /** setter for version of appliance
+   * @param value the version of appliance that should be enabled
+   */
   def setVersion(value: String):Unit = version = value
 
+  /** creates innner xml of IQ
+   */
   def getChildElementXML:String = {
     val ns = ApplianceEnableProvider.namespace
     <appliance xmlns={ ns }>
@@ -28,6 +51,10 @@ class ApplianceEnable extends IQ {
     </appliance>.toString
   }
 
+  /** creates response to this request
+   * @param the enabled applinance
+   * @return the response
+   */
   def createResultIQ(appliance: Appliance):ReplyApplianceEnable = {
     val applianceEnable = new ReplyApplianceEnable
     applianceEnable setPacketID getPacketID
