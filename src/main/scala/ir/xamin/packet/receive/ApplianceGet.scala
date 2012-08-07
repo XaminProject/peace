2eff3f9bd6dae1b397b7bc6f4a8afc4537b0f062
behrooz shabani (everplays) <behrooz@rock.com>
2012-08-07 17:20:38 +0430

    added apidoc of receive ApplianceGet

--- src/main/scala/ir/xamin/packet/receive/ApplianceGet.scala
+++ src/main/scala/ir/xamin/packet/receive/ApplianceGet.scala
@@ -6,20 +6,38 @@ import ir.xamin.Appliance
 import scala.xml._
 import org.jivesoftware.smack.packet.{IQ, Packet}
 
+/** a class that ables users to get information about an appliance
+ */
 class ApplianceGet extends IQ {
   private var name:String = _
   private var version:String = _
 
+  // set type of iq
   setType(IQ.Type.GET)
 
+  /** getter for name of appliance
+   * @return a string which is name of appliance
+   */
   def getName = name
 
+  /** setter for name of appliance
+   * @param value name of appliance
+   */
   def setName(value: String):Unit = name = value
 
+  /** getter for version of appliance
+   * @return a string which is version of appliance
+   */
   def getVersion = version
 
+  /** setter for version of appliance
+   * @param value version of appliance
+   */
   def setVersion(value: String):Unit = version = value
 
+  /** creates inner xml of iq
+   * @return a string which is inner xml of iq
+   */
   def getChildElementXML:String = {
     val ns = ApplianceGetProvider.namespace
     <appliance xmlns={ ns }>
@@ -28,6 +46,10 @@ class ApplianceGet extends IQ {
     </appliance>.toString
   }
 
+  /** creates response iq of request
+   * @param appliance the appliance that user requested to get info of
+   * @return the response
+   */
   def createResultIQ(appliance: Appliance):ReplyApplianceGet = {
     val applianceGet = new ReplyApplianceGet
     applianceGet.setPacketID(getPacketID())
