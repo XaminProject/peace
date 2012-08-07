5f58aceaee2cdc9cb7ba898e630956465a8bff08
behrooz shabani (everplays) <behrooz@rock.com>
2012-08-07 18:23:01 +0430

    added apidoc of receive MarketRemove

--- src/main/scala/ir/xamin/packet/receive/MarketRemove.scala
+++ src/main/scala/ir/xamin/packet/receive/MarketRemove.scala
@@ -6,20 +6,40 @@ import scala.xml._
 import scala.collection.mutable.MutableList
 import org.jivesoftware.smack.packet.{IQ, Packet}
 
+/** a class which represents the stanza that market sends us
+ * when user requests to remove an appliance from his/her
+ * archipel
+ */
 class MarketRemove extends IQ {
   private var remove:MutableList[Tuple2[String, String]] = _
   private var archipel:String = _
 
+  // set type of iq
   setType(IQ.Type.SET)
 
+  /** getter for appliances that user requested to remove
+   * @return a list of appliances to remove
+   */
   def getRemove = remove
 
+  /** setter for appliances that user requested to remove
+   * @param v a list of appliances to be removed
+   */
   def setRemove(v:MutableList[Tuple2[String, String]]) = remove = v
 
+  /** getter for jid of target archipel
+   * @return a string which is jid of target archipel
+   */
   def getArchipel = archipel
 
+  /** setter for jid of target archipel
+   * @param v a string which is jid of target archipel
+   */
   def setArchipel(v:String) = archipel = v
 
+  /** creates inner xml of iq
+   * @return a string which is going to be inner of iq
+   */
   def getChildElementXML:String = {
     val ns = MarketInstallProvider.namespace
     var applianceElements = MutableList[Elem]()
@@ -34,6 +54,9 @@ class MarketRemove extends IQ {
     </remove>.toString
   }
 
+  /** creates reply for request
+   * @return the reply
+   */
   def createResultIQ():ReplyMarketRemove = {
     val marketRemove = new ReplyMarketRemove
     marketRemove setPacketID getPacketID
