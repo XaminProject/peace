ad81729b56c7acbf2b65cc110168f3fda17d4aaf
behrooz shabani (everplays) <behrooz@rock.com>
2012-08-07 19:03:15 +0430

    added apidoc of independent packets

--- src/main/scala/ir/xamin/packet/ApplianceItem.scala
+++ src/main/scala/ir/xamin/packet/ApplianceItem.scala
@@ -3,8 +3,13 @@ package ir.xamin.packet
 import scala.xml._
 import org.jivesoftware.smackx.pubsub.Item
 
+/** this class will be used to provide inner xml of pubsub item
+ * that will be published
+ */
 case class ApplianceItem(version:String) extends Item()
 {
+  /** creates the inner xml of item
+   */
   override def toXML = <item>
       <appliance xmlns="appliance:update:xamin">
         <version>{version}</version>
--- src/main/scala/ir/xamin/packet/OwnerBehalfSubscribe.scala
+++ src/main/scala/ir/xamin/packet/OwnerBehalfSubscribe.scala
@@ -4,11 +4,16 @@ import scala.xml._
 import org.jivesoftware.smack.XMPPConnection
 import org.jivesoftware.smack.packet.Packet
 
+/** this class creates the packet that we need to send to xmpp server
+ * to be able to subscribe another jid to a pubsub
+ */
 class OwnerBehalfSubscribe(xmpp: XMPPConnection, aJid:String, aNode:String) extends Packet {
   protected val to:String = "pubsub."+xmpp.getServiceName()
   protected val node = aNode
   protected val jid = aJid
 
+  /** creates inner xml of packet
+   */
   override def toXML:String = {
     <iq type='set' to={to}>
       <pubsub xmlns='http://jabber.org/protocol/pubsub#owner'>
